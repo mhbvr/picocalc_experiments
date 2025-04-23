a9ad4e4e736eef0b1b1fc8b4330459a9623a18c7
Mikhail Kulemin <mih@mrphs.cc>
2025-04-23 13:51:03 +0100

    Functional repl editor.
    Done:
    - Left/Right
    - Home/End
    - Del/Backspace
    - History of commands
    - Up/Down keys for history navigation
    - Hardware vertical scrolling.
    
    There is a bug in history present when using historical inputs.

--- repl-editor/repl-editor.ino
+++ repl-editor/repl-editor.ino
@@ -50,6 +50,10 @@
 PCKeyboard pc_kbd;
 TFT_eSPI tft;
 
+
+// Linked list of text lines.
+// Used to store history of inputs
+
 struct line {
   char *buffer;
   int size;
@@ -57,37 +61,63 @@ struct line {
   line *prev;
 };
 
-// Add empty line to after the curr one.
-struct line *new_line(line *curr) {
-  line *res = (line *)malloc(sizeof(line));
-  memset(res, 0, sizeof(line));
-  res->prev = curr;
-  res->next = curr->next;
-  curr->next = res;
-  if (curr->next != NULL) {
-    curr->next->prev = res;
+// Initialize new line with (zeroed).
+struct line *init_line() {
+  struct line *l = (struct line *)malloc(sizeof(struct line));
+  memset(l, 0, sizeof(struct line));
+  return l;
+}
+
+void free_line(struct line *l) {
+  free(l->buffer);
+  free(l);
+}
+
+// Insert line to the list after current and return poiter to the new line
+struct line *insert_line(struct line *l, struct line *n) {
+  if (l == NULL) return n;
+
+  n->next = l->next;
+  n->prev = l;
+
+  if (l->next != NULL) l->next->prev = n;
+  l->next = n;
+  
+  return n;
+}
+
+// Remove line from the list.
+struct line *extract_line(struct line *l) {
+  struct line *res = NULL;
+  if (l->prev != NULL) {
+    l->prev->next = l->next;
+    res = l->prev;
   }
+  if (l->next != NULL) {
+    l->next->prev = l->prev;
+    res = l->next;
+  }
+  l->next = NULL;
+  l->prev = NULL;
   return res;
 }
 
-void del_line(line *curr) {
-  if (curr->prev != NULL) curr->prev->next = curr->next;
-  if (curr->next != NULL) curr->next->prev = curr->prev;
-  free(curr->buffer);
-  free(curr);
+void delete_line(struct line *l) {
+  extract_line(l);
+  free_line(l);
 }
 
-line *first(line *curr) {
-  while (curr->prev != NULL) curr = curr->prev;
-  return curr;
+struct line *first(struct line *l) {
+  while (l->prev != NULL) l = l->prev;
+  return l;
 }
 
-line *last(line *curr) {
-  while (curr->next != NULL) curr = curr->next;
-  return curr;
+struct line *last(struct line *l) {
+  while (l->next != NULL) l = l->next;
+  return l;
 }
 
-void add_char(line *l, char c, int pos) {
+void add_char(struct line *l, char c, int pos) {
   if (pos < 0 || pos > l->size) return;
   l->size++;
   l->buffer = (char *)realloc(l->buffer, sizeof(char) * l->size);
@@ -100,7 +130,7 @@ void add_char(line *l, char c, int pos) {
   l->buffer[pos] = c;
 }
 
-void del_char(line *l, int pos) {
+void del_char(struct line *l, int pos) {
   if (pos < 0 || pos >= l->size) return;
 
   for (int i = pos; i < l->size - 1; i++) {
@@ -111,10 +141,8 @@ void del_char(line *l, int pos) {
   l->buffer = (char *)realloc(l->buffer, sizeof(char) * l->size);
 }
 
-char get_char(line *l, int pos) {
-  if (pos < 0 || pos >= l->size) return 0;
-  return l->buffer[pos];
-}
+
+// Text based screen interface
 
 struct screen {
   // Total number of character lines and colunms on the screen
@@ -135,25 +163,26 @@ struct screen {
   uint32_t text_color;
   uint32_t bg_color;
 
-  // Current text position in characters;
+  // Current cursor position (column, lines) from the top left corner
   uint8_t x;
   uint8_t y;
 
-  // Number of the top pixelline. 
+  // The number of the top pixelline. It is used to implement hardware
+  // vertical scrolling
   uint16_t y_start;
 
   // Number of scrolls
   uint16_t scrolls;
 
   // Hardware scrolling parameters
-  uint16_t scroll_area;
-  uint16_t bfa;
+  uint16_t scroll_area; // Number of pixellines
+  uint16_t bfa;         // Bottom fixed area in pixellines
 };
 
 struct screen *S;
 
-struct screen *new_screen() {
-  screen *s = (screen *)malloc(sizeof(screen));
+struct screen *init_screen() {
+  struct screen *s = (screen *)malloc(sizeof(screen));
   memset(s, 0, sizeof(screen));
 
   s->char_height = 14;
@@ -207,14 +236,14 @@ void set_colors(struct screen *s, uint32_t text, uint32_t bg) {
   s->bg_color = bg;
 }
 
-void scroll(screen *s) {
+void scroll_up(screen *s) {
   // Clear top line
   tft.fillRect(0, s->y_start, SCREEN_WIDTH, s->char_height + s->line_dist, s->bg_color);
   
   s->y_start = s->y_start + s->char_height + s->line_dist;
 
-  // Wrapping arond the scroll
-  // Will be more complecated with non zero TFA and BFA
+  // Wrapping around the scroll
+  // Will be more complicated with non zero TFA and BFA
   if (s->y_start >= s->scroll_area) s->y_start = s->y_start - s->scroll_area;
   //if (s->y_start >= s->scroll_area) s->y_start = 0;
 
@@ -223,36 +252,90 @@ void scroll(screen *s) {
   tft.writedata((s->y_start) & 0xFF);
 }
 
-void next_line(struct screen *s, bool scrl) {
-  if (s->y == s->lines - 1) {
-    if (!scrl) return; 
-    scroll(s);
-    s->scrolls++;
+// Move cursor to the prev line, 
+// TODO: support scroll down
+int move_prev_line(struct screen *s, int allow_scroll) {
+  if (s->y > 0) { //
+    s->y--;
+  }
+  return 0;
+}
+
+// Move cursor to the next line, scroll screen up if necessary and allowed.
+int move_next_line(struct screen *s, int allow_scroll) {
+  int scroll = 0;
+  if (s->y == s->lines - 1) { //
+    if (!allow_scroll) return 0; 
+    scroll_up(s);
+    scroll = -1;
   } else {
-    s->y++;   
+    s->y++;
+  }
+  return scroll;
+}
+
+// Move cursor right with horizontal wrapping.
+// Scroll screen up if necessary and allowed.
+int move_right(struct screen *s, int allow_scroll) {
+  if (s->x < s->columns - 1) {
+    s->x++;
+    return 0;
   }
 
   s->x = 0;
+  return move_next_line(s, allow_scroll);
 }
 
-void write(struct screen *s, char c, bool move, bool scrl) {
+// Move cursor left with horizontal wrapping.
+// TODO: Support scroll down. 
+int move_left (struct screen *s, int allow_scroll /* Not implemented */) {
+  if (s->x > 0) {
+    s->x--;
+    return 0;
+  }
+
+  s->x = s->columns - 1;
+  return move_prev_line(s, allow_scroll);
+}
+
+// Write character at the cursor position. If move != 0 move cursor left.
+// Go to the next line at the end of the line when allow_scroll != 0.
+// It returns change of Y coodinate in the case of scrolling. Y < 0 means scrolling up.
+int write(struct screen *s, char c, int move, int allow_scroll) {
   int pos_x = s->x * s->char_width;
   int pos_y = (s->y_start + s->y * (s->char_height + s->line_dist)) % s->scroll_area;
-  // Need to make a background 
+  // Need to make a background as drawChar with GFX font ignores background color.
   tft.fillRect(pos_x, pos_y, s->char_width, s->char_height, s->bg_color);
   tft.drawChar(pos_x, pos_y, c, s->text_color, s->bg_color, 1);
 
-  if (!move) return;
+  if (!move) return 0;
 
-  if (s->x < s->columns - 1) {
-    s->x++;
-    return;
-  }
-
-  next_line(s, scrl);
+  return move_right(s, allow_scroll);
 }
 
-void cursor(struct screen *s, char c, bool show) {
+struct repl {
+  struct line *line;
+  uint8_t num_lines;
+
+  // Cursor position in the line
+  int pos;
+
+  // Position of the line start on the screen.
+  // start_y should be updated after scrolling.
+  uint8_t start_x;
+  uint8_t start_y;
+};
+
+struct repl *init_repl() {
+  struct repl *r = (struct repl*)malloc(sizeof(struct repl));
+  if (r == NULL) return r;
+  memset(r, 0, sizeof(struct repl));
+
+  return r;
+}
+
+// Cursor is an inverse symbol. 
+void draw_cursor(struct screen *s, char c, bool show) {
   uint32_t text_color = s->text_color;
   uint32_t bg_color = s->bg_color;
 
@@ -267,18 +350,182 @@ void cursor(struct screen *s, char c, bool show) {
   }
 }
 
-struct repl {
-  line * current_line;
-  uint8_t num_lines;
+// Used to print data on screen.
+// Return space char when line is NULL or position out of bound.
+char get_c(struct repl *r) {
+  if (r->line == NULL || r->pos >= r->line->size || r->pos < 0) return ' ';
+  return r->line->buffer[r->pos];
+}
 
-  // Cursor position in the line
-  int pos;
+// Returns X position on the screen of the current text symbol.
+int get_x(struct repl *r, struct screen *s) {
+  return (r->start_x + r->pos) % s->columns;
+}
 
-  // Position of the line start on the screen.
-  // start_y should be updated after scrolling.
-  uint8_t start_x;
-  uint8_t start_y;
-};
+// Returns Y position on the screen of the current text symbol.
+int get_y(struct repl *r, struct screen *s) {
+  return r->start_y + (r->start_x + r->pos) / s->columns;
+}
+
+// Length of the current line
+int get_len(struct repl *r) {
+  if (r->line == NULL) return 0;
+  return r->line->size;
+}
+
+void move_cursor(struct repl *r, struct screen *s, char key) {
+  draw_cursor(s, get_c(r), 0);
+  
+  switch(key) {
+  case ARROW_LEFT:
+    if (r->pos == 0) break;  // line start
+    r->pos--;
+    move_left(s, 0);  // Scrolling not allowed
+    break;
+  case ARROW_RIGHT:
+    if (r->pos == get_len(r)) break; // position after line end
+    r->pos++;
+    move_right(s, 0); // Scrolling not allowed
+    break;
+  case HOME:
+    r->pos = 0; 
+    set_position(s, r->start_x, r->start_y);
+    break;
+  case END:
+    r->pos = get_len(r);
+    set_position(s, get_x(r, s), get_y(r, s));
+    break;
+  }
+
+  draw_cursor(s, get_c(r), 1);
+}
+
+void print_line(struct repl *r, struct screen *s, int size) {
+  int x = get_x(r, s);
+  int y = get_y(r, s);
+  int pos = r->pos;
+  int dy = 0;
+  while (r->pos < size) {
+    dy += write(s, get_c(r), 1, 1); 
+    r->pos++;
+  }
+  // Adjust start of line because of scrolling
+  r->start_y += dy;
+  // Return cursor to the original positions
+  r->pos = pos;
+  set_position(s, x, y + dy);
+}
+
+void show_line(struct repl *r, struct screen *s, struct line *l) {
+  // Go to the line start
+  r->pos = 0;
+  set_position(s, r->start_x, r->start_y);
+  int old_len = get_len(r);
+  r->line = l;
+  // Clear the scroll counter to track line start
+  // and print new line
+  print_line(r, s, max(get_len(r), old_len) + 1);
+}
+
+bool is_printable(char c) {
+  if (c >= 0x20 && c <= 0x7e) return true;
+  return false;
+}
+
+char * read_line(struct repl *r, struct screen *s) {
+  if (r->line == NULL || r->line->size > 0) {
+    // First run on non empty prev input
+    r->line = insert_line(r->line, init_line());
+  }
+
+  write(s, '>', 1, 1);
+  write(s, ' ', 1, 1);
+  draw_cursor(s, ' ', 1);
+  
+  r->pos = 0;
+  r->start_x = s->x;
+  r->start_y = s->y;
+
+  while (1) {
+    if (pc_kbd.keyCount() > 0) {
+      PCKeyboard::KeyEvent event = pc_kbd.keyEvent();
+      if (event.state ==  PCKeyboard::StatePress) {
+        
+        if (is_printable(event.key)) {
+          draw_cursor(s, get_c(r), 0);
+          add_char(r->line, event.key, r->pos);
+          print_line(r, s, get_len(r) + 1);
+          r->pos++;
+          move_right(s, 1);
+          draw_cursor(s, get_c(r), 1);
+          continue;
+        }
+
+        switch(event.key) {
+        case ARROW_UP:
+          // TODO: Prev input from history
+          if (r->line->prev == NULL) break;
+          show_line(r, s, r->line->prev);
+          draw_cursor(s, get_c(r), 1);
+          break;
+        case ARROW_DOWN:
+          // TODO: Next input from history
+          if (r->line->next == NULL) break;
+          show_line(r, s, r->line->next);
+          draw_cursor(s, get_c(r), 1);
+          break;
+        case ARROW_LEFT:
+        case ARROW_RIGHT:
+        case HOME:
+        case END:
+          move_cursor(r, s, event.key);
+          break;
+        case DELETE:
+          // Delete symbol in the cursor position
+          if (r->pos == get_len(r)) continue;
+          del_char(r->line, r->pos);
+          print_line(r, s, get_len(r) + 1);
+          draw_cursor(s, get_c(r), 1); 
+          break;
+        case BACKSPACE:
+          // Delete symbol before cursor
+          if (r->pos == 0) continue;
+          if (r->pos == get_len(r)) draw_cursor(s, ' ', 0);
+          r->pos--;
+          move_left(s, 1);
+          del_char(r->line, r->pos);
+          print_line(r, s, get_len(r) + 1);
+          draw_cursor(s, get_c(r), 1); 
+          break;
+        case ENTER:
+          // TODO: 0 at the end, history.
+          // Clear cursor
+          draw_cursor(s, get_c(r), 0);
+
+          // Move to the end of line
+          r->pos = get_len(r);
+          set_position(s, get_x(r, s), get_y(r, s));
+
+          // Move to the end of the next line
+          move_next_line(s, 1);
+          s->x = 0;
+
+          if (r->line->next != NULL) {
+            // Edited history line
+            struct line *current = r->line;
+            r->line = extract_line(current);
+            r->line = last(r->line);
+            r->line = insert_line(r->line, current);
+          }
+          return r->line->buffer;
+          break;
+        }
+      }
+    }
+  }
+}
+
+struct repl *R;
 
 
 void setup() {
@@ -288,35 +535,10 @@ void setup() {
   Wire1.setClock(10000);
   Wire1.begin();  
   pc_kbd.begin(PCKEYBOARD_DEFAULT_ADDR, &Wire1);
-  S = new_screen();
-  cursor(S, ' ', true);
-}
-
-bool is_printable(char c) {
-  if (c >= 0x20 && c <= 0x7e) return true;
-  return false;
+  S = init_screen();
+  R = init_repl();
 }
 
 void loop() {
-  if (pc_kbd.keyCount() > 0) {
-    PCKeyboard::KeyEvent event = pc_kbd.keyEvent();
-    if (event.state ==  PCKeyboard::StatePress) {
-      if (is_printable(event.key)) {
-        // cursor(S, ' ', false);
-        write(S, event.key, true, true);
-        cursor(S, ' ', true);
-        return;
-      }
-
-      switch(event.key) {
-      case ENTER:
-        cursor(S, ' ', false);
-        next_line(S, true);
-        cursor(S, ' ', true);
-        break;
-      case ESC:
-        scroll(S);
-      }
-    }
-  }
+  read_line(R, S);
 }
